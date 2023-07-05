76e58032365c891411ec1b628e8032f0d92aee46
이종호 <substantiation@naver.com>
2023-07-05 16:42:03 +0900

    3-2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,34 @@
-import HeaderComponent from "@/components/common/Header";
-import React, { Fragment } from "react";
+import { Fragment } from "react";
+import Header from "../components/common/Header";
 import styles from "../styles/header.module.scss";
 import Link from "next/link";
-import { AiOutlineShareAlt } from "react-icons/ai";
 import { VscFeedback } from "react-icons/vsc";
+import { AiOutlineShareAlt } from "react-icons/ai";
+import MapSection from "../components/home/MapSection";
+
 export default function Home() {
   return (
     <Fragment>
-      <HeaderComponent
+      <Header
         rightElements={[
           <button
-            onClick={() => alert("복사")}
+            onClick={() => {
+              alert("복사!");
+            }}
             className={styles.box}
             style={{ marginRight: 8 }}
             key="button"
           >
             <AiOutlineShareAlt size={20} />
           </button>,
-          <Link href={"/feedback"} className={styles.box} key="box">
+          <Link href="/feedback" className={styles.box} key="link">
             <VscFeedback size={20} />
           </Link>,
         ]}
       />
+      <main style={{ width: "100%", height: "100%" }}>
+        <MapSection />
+      </main>
     </Fragment>
   );
 }
