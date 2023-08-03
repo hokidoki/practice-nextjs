f5ee58be7e27e391db41850108383436c32d2b3a
이종호 <substantiation@naver.com>
2023-08-03 10:04:00 +0900

    indexpage redirect to "/contents"

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const redirects = async () => [
+  {
+    source : "/",
+    destination : "/contents",
+    permanent : false
+  }
+]
+
 const nextConfig = {
   reactStrictMode: true,
+  redirects
 }
 
 module.exports = nextConfig
