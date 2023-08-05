4b00181210baf27bd822bd8f9c41f501ad27daaa
이종호 <substantiation@naver.com>
2023-08-05 23:59:02 +0900

    API setup

---
+++ src/api/handler/contents.ts
@@ -0,0 +1,24 @@
+
+import { DELETE, GET, POST, PUT } from "../base"
+import { pathmaker } from "../utils"
+import { MinContent, Content, Comment } from "@/types/api";
+
+const BASE_PATH = `/contents`
+const path = pathmaker(BASE_PATH);
+
+// Contents
+export const getContents = () => GET<MinContent[]>(path(""));
+export const getContent = (id: string) => GET<Content>(path(`/${id}`));
+export const postContent = (content: Content) => POST(path(`/`), content);
+export const putContent = (content: Content) => PUT(path(`/${content.id}`), content);
+export const deleteContent = (id: string) => DELETE(path(`/${id}`));
+
+// Comments
+export const getComments = (contentId: string) => GET<Comment[]>(path(contentId, "comments"));
+export const postComment = (comment: Comment) => POST(path(comment.articleId, "comments"), comment);
+export const putComment = (content: Content) => PUT(path(content.id), content);
+export const deleteComment = ({ contentId, commentId }: { contentId: string, commentId: string }) => DELETE(path(contentId, commentId));
+
+
+
+
