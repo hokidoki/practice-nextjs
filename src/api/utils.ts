0747b5e696f3048eb74876716e4e96b529eed828
이종호 <substantiation@naver.com>
2023-08-07 08:47:10 +0900

    update api utils & paths

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -4,4 +4,4 @@ import { curry, join } from "@fxts/core";
 /**
  * API Handler에서 사용되는 path 생성기
  */
-export const pathmaker = (base_path: string) => (...args: string[]) => base_path + curry(join("/"))(args);
+export const pathmaker = (base_path: string) => (...args: string[]) => join("/", [base_path, ...args])
