96fd537b2ee5f5c8515d380e17d7e7c46be8dcbb
이종호 <substantiation@naver.com>
2023-08-09 14:24:46 +0900

    test setup and test CommentEditor

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/src/$1',
+      "@render": "<rootDir>/test-utils/render.tsx"
+    },
+    testEnvironment: 'jest-environment-jsdom',
+    roots: ['<rootDir>/src'],
+  };
+
+module.exports = createJestConfig(customJestConfig)
